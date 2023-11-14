ad9ed99d3c83376e55cf6314ec28363a41f8847d
bonggyoson <bonggyoson@gmail.com>
2023-11-14 10:53:38 +0900

    Initial commit

---
+++ src/main/java/com/example/JspMybatisSample/JspMybatisSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.JspMybatisSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspMybatisSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspMybatisSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JspMybatisSample/JspMybatisSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JspMybatisSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspMybatisSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
