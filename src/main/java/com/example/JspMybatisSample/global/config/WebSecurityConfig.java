1675058896a45383c691bc0a047509654e9c6ea6
bonggyoson <bonggyoson@gmail.com>
2023-12-13 13:07:23 +0900

    feat : spring security 추가 및 설정 작업중

---
+++ src/main/java/com/example/JspMybatisSample/global/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.JspMybatisSample.global.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()    // CSRF 공격 방어 해제
+            .authorizeRequests()        // URI에 따른 페이지 권한 부여 메소드, antMatchers 기능을 위한 메소드
+            .antMatchers("/article/**").authenticated()         // 특정 URL 접근 시 인가가 필요한 URI
+            .anyRequest().permitAll()   // 특정 URI을 제외한 나머지 URI은 전부 인가
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .loginProcessingUrl("/loginProc")
+            .defaultSuccessUrl("/")
+            .and()
+            .build();
+    }
+
+}
