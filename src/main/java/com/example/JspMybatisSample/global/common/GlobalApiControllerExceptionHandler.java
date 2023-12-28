842c8b7473bb84f0bea3c9006f5cf8d23899747e
bonggyoson <bonggyoson@gmail.com>
2023-12-28 17:29:09 +0900

    feat : GlobalApiControllerExceptionHandler 작업 중

--- src/main/java/com/example/JspMybatisSample/global/common/GlobalApiControllerExceptionHandler.java
+++ src/main/java/com/example/JspMybatisSample/global/common/GlobalApiControllerExceptionHandler.java
@@ -1,8 +1,19 @@
 package com.example.JspMybatisSample.global.common;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalApiControllerExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CommonResponse<ErrorResponse<?>>> handleValidationException(
+        MethodArgumentNotValidException exception) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(CommonResponse.res(exception.getMessage()));
+    }
 }
