9e73546f5fce16a4d3bc72f242cd941618158de8
bonggyoson <bonggyoson@gmail.com>
2024-01-11 12:47:48 +0900

    fix : schema.sql comment 추가 및 article 테이블 수정

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -9,6 +9,7 @@ CREATE SEQUENCE board.seq_article START 1;
 CREATE TABLE board.article
 (
     article_id         int8                  DEFAULT nextval('board.seq_article') NOT NULL,
+    member_id          int8         NOT NULL,
     article_title      varchar(255) NOT NULL,
     article_content    text         NOT NULL,
     article_writer     varchar(255) NOT NULL,
@@ -33,5 +34,23 @@ CREATE TABLE board.member
     member_name     varchar(255)                             NOT NULL,
     member_role     varchar(255)                             NOT NULL,
     created_at      timestamp                                NOT NULL,
+    updated_at      timestamp
+);
+
+-- board.comment definition
+
+-- Drop table
+
+DROP TABLE board.comment;
+
+CREATE SEQUENCE board.seq_comment START 1;
+
+CREATE TABLE board.comment
+(
+    comment_id      int8 DEFAULT nextval('board.seq_comment') NOT NULL,
+    article_id      int8                                      NOT NULL,
+    member_id       int8                                      NOT NULL,
+    comment_content text                                      NOT NULL,
+    created_at      timestamp                                 NOT NULL,
     updated_at      timestamp
 );
