3688e5f47694de2da1e5e4049f5f7c037d5733be
bonggyoson <bonggyoson@gmail.com>
2024-02-02 09:41:42 +0900

    fix : handlebars math helper 수정

--- src/main/resources/static/js/common/helper.js
+++ src/main/resources/static/js/common/helper.js
@@ -37,6 +37,10 @@ Handlebars.registerHelper("math", function (lvalue, operator, rvalue, options) {
   rvalue = parseFloat(rvalue);
 
   return {
-    "+": lvalue + rvalue
+    "+": lvalue + rvalue,
+    "-": lvalue - rvalue,
+    "*": lvalue * rvalue,
+    "/": lvalue / rvalue,
+    "%": lvalue % rvalue
   }[operator];
 });
