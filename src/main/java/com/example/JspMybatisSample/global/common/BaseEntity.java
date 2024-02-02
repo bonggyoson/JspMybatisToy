f3b58983b8e8263a9b322a914a65a691d2d52321
bonggyoson <bonggyoson@gmail.com>
2024-02-02 09:41:57 +0900

    fix : BaseEntity @Getter 어노테이션 추가

--- src/main/java/com/example/JspMybatisSample/global/common/BaseEntity.java
+++ src/main/java/com/example/JspMybatisSample/global/common/BaseEntity.java
@@ -1,7 +1,9 @@
 package com.example.JspMybatisSample.global.common;
 
 import java.time.LocalDateTime;
+import lombok.Getter;
 
+@Getter
 public class BaseEntity {
 
     private LocalDateTime createdAt;
